32c1f8e5333c64a101b216f3597ad6c8c56c5ee9
hamza <hamzabousseta1@gmail.com>
2023-06-08 16:42:14 -0700

    mandatory taskk  with updated index

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
 
-length = len(argv) - 1
-args = argv[1:]
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
 
-print("{} argument{}{}".format(length, 's' if length != 1 else '', ':' if length > 0 else '.'))
-
-for i, arg in enumerate(args, start=1):
-    print("{}: {}".format(i, arg))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
