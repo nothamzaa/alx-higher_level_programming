1f97326fcb3b2868e1aa5c7a1ac6cd7711bfb5aa
hamza <hamzabousseta1@gmail.com>
2023-06-08 16:09:51 -0700

    task 0

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,2 +1,14 @@
-def add(a, b):
-    return a + b
+#!/usr/bin/python3
+
+import add_0
+
+
+def main():
+    a = 1
+    b = 2
+    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
+
+if __name__ == "__main__":
+    main()
